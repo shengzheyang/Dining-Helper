c763d5c50a29c3f4abb17774312cc8b8259f08bf
Vaibhav Yengul <vyengul@gmail.com>
2019-11-16 13:01:29 -0800

    test3

--- public/like_button.js
+++ public/like_button.js
@@ -3,23 +3,15 @@
 const e = React.createElement;
 
 class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+	constructor(props) {
+	    super(props);
+	    this.state = { liked: false };
+	}
+
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+    return (<h1>Hello World</h1>);
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(LikeButton), document.getElementById("main"));
